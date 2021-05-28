7b71eed3d7f32d10d94fbfb2ea131c4bb4b79934
Tiana Milanovich <tianarose.m@outlook.com>
2021-05-28 15:17:11 +0100

    done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -19,13 +19,13 @@ end
 def create
   @list = List.new(list_params)
     if @list.save
-      redirect_to lists_path
+      redirect_to list_path(@list)
     else
       render :new
+
     end
+
 end
-
-
 
   private
    def list_params
